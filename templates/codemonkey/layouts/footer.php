639899362f5fa8a24d0194a3d555cc7e9dc3dcd8
Gersón Gómez <ggomez@codemonkey.com.mx>
2021-01-16 22:10:32 -0500

    Actualización

--- templates/codemonkey/layouts/footer.php
+++ templates/codemonkey/layouts/footer.php
@@ -1,9 +1,13 @@
 <?php defined('_EXEC') or die; ?>
 
         <footer class="main_footer">
-            <div class="container">
-                <p>Copyright © 2021 <strong><?php echo Configuration::$web_page; ?></strong> <i class="mdi mdi-heart text-danger"></i> {$lang.development_by} <a href="https://codemonkey.com.mx" target="_blank">codemonkey.com.mx</a></p>
-            </div>
+            <nav>
+                <ul>
+                    <li><a href="https://facebook.com" target="_blank"><i class="fab fa-facebook"></i></a></li>
+                    <li><a href="https://instagram.com" target="_blank"><i class="fab fa-instagram"></i></a></li>
+                    <li>Copyright © 2021 <strong><?php echo Configuration::$web_page; ?></strong> <i class="mdi mdi-heart text-danger"></i> {$lang.development_by} <a href="https://codemonkey.com.mx" target="_blank">codemonkey.com.mx</a></li>
+                </ul>
+            </nav>
         </footer>
         <script src="{$path.js}jquery-3.4.1.min.js"></script>
         <script src="https://cdn.codemonkey.com.mx/js/valkyrie.js?v=1.0"></script>
